07c44f191cceb340c27eec54510e907102120f06
IsaacThomas245 <114020944+IsaacThomas245@users.noreply.github.com>
2023-07-26 17:15:16 -0500

    first commit

---
+++ src/secret.py
@@ -0,0 +1,8 @@
+# For the YouTube Data API
+PLAYLIST_ID = "PLqqrBkAAeqhNGlRf3x2Cp1qDO1Q1MngZK"
+
+# For the Spotify Web API
+CLIENT_ID = "c127b095d65a41da804f1ce36dbf3563"
+CLIENT_SECRET = "41a304d487ad44c8b91f281a48936aba"
+USER_ID = "22m2umnyfnnugs6kacn2w5ngq"
+REDIRECT_URI = "https://www.google.com"
